f5c80438bad1a2d8341feaa0bc2cc125faf3c5ae
EmilyLow <emlow@me.com>
2020-10-14 19:58:19 -0500

    Get works

--- api/server.js
+++ api/server.js
@@ -6,4 +6,17 @@ const server = express();
 
 server.use(express.json());
 
+//Attempting to do it directly in this file with server.get instead of router.get ect.
+//?? Unclear where the base url is/should-be defined
+server.get('/accounts', (req, res) => {
+    db('accounts')
+    .then(accounts => {
+      res.json(accounts);
+    }) 
+    .catch (err => {
+      res.status(500).json({ message: 'Failed to get accounts' });
+    });
+  });
+
+
 module.exports = server;
--- index.js
+++ index.js
@@ -5,3 +5,6 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`\n== API running on port ${PORT} ==\n`);
 });
+//?? I'm unclear when we need to use server.use(routerName). Only if we have more than one relevant file?
+//?? Basically, when we are setting the server via express and when we are using said server ect. 
+//?? Also confused if routers are only required if there is a non-server file
