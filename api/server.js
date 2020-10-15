9d1388620583e40397ecf24fa95718953c0ebc78
EmilyLow <emlow@me.com>
2020-10-14 20:12:14 -0500

    Post works

--- api/server.js
+++ api/server.js
@@ -9,7 +9,8 @@ server.use(express.json());
 //Attempting to do it directly in this file with server.get instead of router.get ect.
 //?? Unclear where the base url is/should-be defined
 server.get('/accounts', (req, res) => {
-    db('accounts')
+    // db('accounts')
+    db.select('*').from('accounts')
     .then(accounts => {
       res.json(accounts);
     }) 
@@ -18,5 +19,31 @@ server.get('/accounts', (req, res) => {
     });
   });
 
+ server.post('/accounts', async (req, res, next) => {
+     try{
+         const payload = {
+             name: req.body.name,
+             budget: req.body.budget
+
+         }
+
+         if(!payload.name || !payload.budget) {
+             res.status(400).json({message: "Inclulde name and budget"})
+         }
+
+         //? Anything wrong with not assinging this to anything?
+         await db.insert(payload).into('accounts');
+         res.status(201).json("Successfully added");
+     } catch (err) {
+         //??? What is the point of next in this context? What exactly is it doing here
+        next(err);
+     }
+
+ })
+
+server.put("/accounts/:id", async (req, res, next) => {
+
+    
+})
 
 module.exports = server;
